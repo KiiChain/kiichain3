cc1535efedb99a902d7fde5140662a25a444b320
Jhelison Uchoa <jhelisong@gmail.com>
2025-02-12 16:10:40 -0300

    feat: add max inflation and yearly inflation to the mint module

--- x/mint/gov.go
+++ x/mint/gov.go
@@ -6,11 +6,21 @@ import (
 	"github.com/kiichain/kiichain3/x/mint/types"
 )
 
+// HandleUpdateMinterProposal handle the update minter governance proposal
 func HandleUpdateMinterProposal(ctx sdk.Context, k *keeper.Keeper, p *types.UpdateMinterProposal) error {
+	// Validate the minter object
+	// This validates mint total, mint dates and mint denom
 	err := types.ValidateMinter(*p.Minter)
 	if err != nil {
 		return err
 	}
+
+	// Validate the inflation rate
+	if err := k.ValidateInflationRate(ctx, p.Minter); err != nil {
+		return err
+	}
+
+	// Set the minter
 	k.SetMinter(ctx, *p.Minter)
 	return nil
 }
---
+++ x/mint/keeper/inflation.go
@@ -0,0 +1,63 @@
+package keeper
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/kiichain/kiichain3/x/mint/types"
+)
+
+// ValidateMinter validates a new minter inflation rate
+func (k Keeper) ValidateInflationRate(ctx sdk.Context, newMinter *types.Minter) error {
+	// Get the inflation rate from params
+	inflationMax := k.GetParams(ctx).InflationMax
+
+	// Get the inflation rate for the new minter
+	newInflationRate, err := k.GetAnnualInflationForMint(ctx, newMinter)
+	if err != nil {
+		return err
+	}
+
+	// Ensure the new inflation rate does not exceed the max allowed inflation
+	if newInflationRate.GT(inflationMax) {
+		return fmt.Errorf("annual inflation rate %.6f exceeds maximum allowed inflation rate of %.6f", newInflationRate, inflationMax)
+	}
+
+	// Return nil if we reach here
+	return nil
+}
+
+// GetAnnualInflationForMint returns the annual inflation for
+func (k Keeper) GetAnnualInflationForMint(ctx sdk.Context, minter *types.Minter) (sdk.Dec, error) {
+	// Get the current supply for the token that will be minted
+	supply := k.bankKeeper.GetSupply(ctx, minter.Denom).Amount.ToDec()
+
+	// Get the end date and start date
+	startDate, err := minter.GetStartDateTime()
+	if err != nil {
+		return sdk.ZeroDec(), err
+	}
+	endDate, err := minter.GetEndDateTime()
+	if err != nil {
+		return sdk.ZeroDec(), err
+	}
+
+	// Compute total days in the minting period
+	totalDays := types.DaysBetween(startDate, endDate)
+	if totalDays == 0 {
+		return sdk.Dec{}, fmt.Errorf("invalid minting duration: must be greater than zero days")
+	}
+
+	// Convert days to years
+	years := sdk.NewDec(int64(totalDays)).Quo(sdk.NewDec(365))
+
+	// Get the total mintable amount
+	totalMint := sdk.NewDecFromInt(sdk.NewIntFromUint64(minter.TotalMintAmount))
+
+	// Compute the annualized inflation rate
+	annualInflation := totalMint.Quo(years).Quo(supply)
+
+	// Return the annual inflation
+	return annualInflation, nil
+}
