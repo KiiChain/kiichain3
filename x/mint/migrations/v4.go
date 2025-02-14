b4874e3ea008a4924e77d49d0fcb88837e3a3400
Jhelison Uchoa <jhelisong@gmail.com>
2025-02-14 15:13:04 -0300

    feat: add migration and tests

---
+++ x/mint/migrations/v4.go
@@ -0,0 +1,19 @@
+package migrations
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/kiichain/kiichain3/x/mint/keeper"
+	"github.com/kiichain/kiichain3/x/mint/types"
+)
+
+// V4MigrateStore apply the migration from v3 to v4 for the module
+func V4MigrateStore(ctx sdk.Context, k *keeper.Keeper) error {
+	// Set the new parameter with its default value
+	defaultParams := types.DefaultParams()
+	k.SetParams(ctx, defaultParams)
+
+	ctx.Logger().Info("Migration to v4 completed successfully")
+
+	return nil
+}
