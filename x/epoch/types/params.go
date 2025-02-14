793ca58e1fccfb586153b2a04cc89e1375e792de
Jhelison Uchoa <jhelisong@gmail.com>
2025-02-14 13:55:51 -0300

    feat: add new param for max fee

--- x/epoch/types/params.go
+++ x/epoch/types/params.go
@@ -7,6 +7,16 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// Parameter store keys
+var (
+	KeyMaxHooksGasAllowed = []byte("MaxHooksGasAllowed")
+)
+
+// Default values for params
+const (
+	DefaultMaxHooksGasAllowed = 10000000
+)
+
 var _ paramtypes.ParamSet = (*Params)(nil)
 
 // ParamKeyTable the param key table for launch module
@@ -15,31 +25,53 @@ func ParamKeyTable() paramtypes.KeyTable {
 }
 
 // NewParams creates a new Params instance
-func NewParams() Params {
-	return Params{}
+func NewParams(maxHooksGasAllowed uint64) Params {
+	return Params{
+		MaxHooksGasAllowed: maxHooksGasAllowed,
+	}
 }
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(DefaultMaxHooksGasAllowed)
+}
+
+// String implements the Stringer interface
+func (p Params) String() string {
+	out, _ := yaml.Marshal(p)
+	return string(out)
 }
 
 // ParamSetPairs get the params.ParamSet
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{}
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(KeyMaxHooksGasAllowed, &p.MaxHooksGasAllowed, validateMaxHooksGasAllowed),
+	}
 }
 
 // Validate validates the set of params
 func (p Params) Validate() error {
-	if p != NewParams() {
-		return fmt.Errorf("Epoch does not have any params defined")
+	// Validate the max hooks gas allowed
+	if err := validateMaxHooksGasAllowed(p.MaxHooksGasAllowed); err != nil {
+		return err
 	}
 
 	return nil
 }
 
-// String implements the Stringer interface.
-func (p Params) String() string {
-	out, _ := yaml.Marshal(p)
-	return string(out)
+// validateMaxHooksGasAllowed validates the max hooks allowed gas
+func validateMaxHooksGasAllowed(i interface{}) error {
+	// Check the interface
+	maxAllowedGas, ok := i.(uint64)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	// The value should not be zero
+	if maxAllowedGas == 0 {
+		return fmt.Errorf("epoch param max allowed gas can't be zero")
+	}
+
+	// We can't safely set a upper bound, so we return
+	return nil
 }
