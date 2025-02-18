c4fad18b8764fb323964818b4544ae45dad3542e
Andres Ramirez <andres.ramirez9912@gmail.com>
2025-02-17 23:40:06 -0500

    Chore: Add unit test to oracle keeper

--- x/oracle/types/expected_keepers.go
+++ x/oracle/types/expected_keepers.go
@@ -14,7 +14,7 @@ type StakingKeeper interface {
 	TotalBondedTokens(ctx sdk.Context) sdk.Int                                 // Retrieves total staked tokens (useful for slashing calculations)
 	Slash(sdk.Context, sdk.ConsAddress, int64, int64, sdk.Dec)                 // Slashes a validator or delegate who fails to vote in the oracle
 	Jail(ctx sdk.Context, consAddr sdk.ConsAddress)                            // Jail a validator or delegator
-	ValidatorPowerStoreIterator(ctx sdk.Context) sdk.Iterator                  // Used to computing validator rankings or total power
+	ValidatorsPowerStoreIterator(ctx sdk.Context) sdk.Iterator                 // Used to computing validator rankings or total power
 	MaxValidators(ctx sdk.Context) uint32                                      // Return the maximum amount of bonded validators
 	PowerReduction(ctx sdk.Context) (res sdk.Int)                              //Returns the power reduction factor,
 }
@@ -28,9 +28,9 @@ type AccountKeeper interface {
 // BankKeeper is expected keeper for bank module, because I need to handle
 // coins, get balance, receive and send coins
 type BankKeeper interface {
-	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin                                          //Check the oracle module account balance by denom
-	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coin                                                    // Check the oracle module account balance all denom
-	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule string, recipientModule string, amount sdk.Coin) error // Transfer tokens between module accounts (e.g., moving slashed tokens)
+	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin                                           //Check the oracle module account balance by denom
+	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins                                                    // Check the oracle module account balance all denom
+	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule string, recipientModule string, amount sdk.Coins) error // Transfer tokens between module accounts (e.g., moving slashed tokens)
 	GetDenomMetaData(ctx sdk.Context, denom string) (banktypes.Metadata, bool)
 	SetDenomMetaData(ctx sdk.Context, denomMetaData banktypes.Metadata)
 }
