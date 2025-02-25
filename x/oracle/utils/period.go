5c7ed1268c3700a4b32c5f98deefbca5dd412c89
Andres Ramirez <andres.ramirez9912@gmail.com>
2025-02-25 16:02:57 -0500

    Feat: Init midblocker logic, Calculate exchange rate

--- x/oracle/utils/period.go
+++ x/oracle/utils/period.go
@@ -1,5 +1,7 @@
 package utils
 
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
 const (
 	BlocksPerMinute = uint64(75)
 	BlocksPerHour   = BlocksPerMinute * 60
@@ -8,3 +10,10 @@ const (
 	BlocksPerMonth  = BlocksPerDay * 30
 	BlocksPerYear   = BlocksPerDay * 365
 )
+
+// IsPeriodLastBlock checks if the block time on the context means the
+// last block to finish the blocksPerPeriod
+func IsPeriodLastBlock(ctx sdk.Context, blocksPerPeriod uint64) bool {
+	nextBlockHeight := uint64(ctx.BlockHeight() + 1) // Get the next block height
+	return nextBlockHeight%blocksPerPeriod == 0      // Check if the next block height is equal to the blocks per period
+}
