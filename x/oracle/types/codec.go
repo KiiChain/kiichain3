4456152bf0ca4fec54ad0b9aa425912f133a2a6d
Andres Ramirez <andres.ramirez9912@gmail.com>
2025-03-06 11:59:26 -0500

    feat: implement AppModule interfaces on module.go

--- x/oracle/types/codec.go
+++ x/oracle/types/codec.go
@@ -3,14 +3,21 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
-// RegisterCodec register the messages for transactions
+// RegisterCodec registers the messages for transactions
 func RegisterCodec(cdc *codec.LegacyAmino) {
-
+	cdc.RegisterConcrete(&MsgAggregateExchangeRateVote{}, "oracle/MsgAggregateExchangeRateVote", nil)
+	cdc.RegisterConcrete(&MsgDelegateFeedConsent{}, "oracle/MsgDelegateFeedConsent", nil)
 }
 
-// RegisterInterfaces
+// RegisterInterfaces registers the request messages on the tx rpc
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
-
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgAggregateExchangeRateVote{},
+		&MsgDelegateFeedConsent{},
+	)
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
