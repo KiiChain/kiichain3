638fb792e2b50ba592412e2be987a21cb77d0176
Andres Ramirez <andres.ramirez9912@gmail.com>
2025-03-31 09:39:21 -0500

    fix: fix blocks per day calculation

--- x/oracle/utils/period.go
+++ x/oracle/utils/period.go
@@ -5,7 +5,7 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 const (
 	BlocksPerMinute = uint64(75)
 	BlocksPerHour   = BlocksPerMinute * 60
-	BlocksPerDay    = BlocksPerMinute * 24
+	BlocksPerDay    = BlocksPerHour * 24
 	BlocksPerWeek   = BlocksPerDay * 7
 	BlocksPerMonth  = BlocksPerDay * 30
 	BlocksPerYear   = BlocksPerDay * 365
